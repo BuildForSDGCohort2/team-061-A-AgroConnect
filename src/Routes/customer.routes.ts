2d06ee80fdc39b5d65b2001630ef3f5b68e0cccf
mjaynixxz <mmuhammadjamiu@gmail.com>
2020-09-18 20:36:02 +0100

    customer endPoint

--- src/Routes/customer.routes.ts
+++ src/Routes/customer.routes.ts
@@ -1,8 +1,27 @@
 import * as customercontroller from "../Controllers/Customer.controller"
-import express, {Request,Response} from "express"
 import {createResponse} from "../Utils/Response.custom"
-export let Customer_Router = express.Router()
+import express, {Request,Response} from "express"
+export let Customer_router = express.Router()
 
-Customer_Router.all("/info",(req:Request,res:Response)=>{
-    return createResponse(res,"CRUD API For handling Customer operations",{},200)
-})
+Customer_router.all("/info",(req:Request,res:Response)=>{
+    return createResponse(res,"CRUD API For handling Customer operations",{},20Customer
+//get host/farmer/
+Customer_router.get("/",farmercontroller.getAllFarmers)
+
+// //post host/farmer/
+// router.post("/",farmercontroller.createFarmer)
+
+//delete host/farmer/id
+Customer_router.delete("/:id",farmercontroller.deleteFarmer)
+
+//post host/farmer/update
+Customer_router.post("/update",farmercontroller.updateFarmer)
+
+//get host/farmer/:id
+Customer_router.get("/find",farmercontroller.getFarmerbyId)
+
+//post host/farmer/:limit
+Customer_router.post("/:limit",farmercontroller.getFarmers)
+
+//post host/farmer/
+Customer_router.post("/",farmercontroller.getFarmers)
