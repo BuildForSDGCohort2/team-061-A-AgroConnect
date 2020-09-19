9d60ce9fd2ba6e15a0acc08962f9088d8bff7660
Chibueze-20 <analogbeichibueze@gmail.com>
2020-09-19 21:26:47 +0100

    CRUD for farmer product

--- src/Controllers/Product.controller.ts
+++ src/Controllers/Product.controller.ts
@@ -1,3 +1,83 @@
-import {ProductRepository} from "../repositories/Product.repository"
-import {Request, Response} from "express"
+import { ProductRepository } from "../repositories/Product.repository"
+import { Request, Response } from "express"
 import { Product } from "../entities/Product";
+import { getModelForClass } from "@typegoose/typegoose";
+import {createResponse} from "../Utils/Response.custom";
+
+const repository = new ProductRepository(getModelForClass(Product))
+
+export let createProduct = async (req: Request, res: Response) =>{
+    const product = req.body
+    const result = await repository.create(product)
+    if (result) {
+        const newProduct = await repository.findOne({_id:result})
+        return createResponse(res,"Product created",newProduct,200)
+    }else{
+        return createResponse(res,"Error creating product in db",undefined,500)
+    }
+}
+export let deleteProduct = async (req: Request, res: Response) => {
+    const id = req.params.id
+    const result = await repository.delete(id)
+    if (result) {
+        return res.send("Deleted " + id + " successfully")
+    }
+    return res.status(500).send("Delete operation failed")
+}
+export let getAllProducts = async (req: Request, res: Response) => {
+    const query = {}
+    const result = await repository.find(query)
+    if (result) {
+        return createResponse(res,"Products found",result,200)
+    }
+    return createResponse(res,"Fatal error occured",undefined,500)
+}
+export let updateProduct = async (req: Request, res: Response) => {
+    const id = String(req.query.id)
+    const update = req.body
+    const result = await repository.update(id, update)
+    if (Boolean(result)) {
+        // console.log(result.firstname)
+        return createResponse(res,"Product found",result,200)
+    } else {
+        return createResponse(res,"Product not found",undefined,404)
+    }
+}
+export let getProductbyId = async (req: Request, res: Response) => {
+    const id = req.query.id
+    const result = await repository.findOne({ _id: id })
+    // console.log(Boolean(result))
+    if (Boolean(result)) {
+        // console.log(result.firstname)
+        return createResponse(res,"Product found",result,200)
+    } else {
+        return createResponse(res,"Product not found",undefined,404)
+    }
+}
+export let getProducts = async (req: Request, res: Response) => {
+    const query = req.body
+    // console.log(query)
+    const result = await repository.find(query)
+    console.log(Boolean(result))
+    if (result) {
+        console.log("done")
+        return createResponse(res,"Products found",result,200)
+    } else {
+        return createResponse(res,"Products not found",undefined,404)
+    }
+}
+export let getNProducts = async (req: Request, res:Response)=>{
+    const query = req.body
+    if (req.params.limit !="") {
+        const limit = Number(req.params.limit)
+        const result = await repository.findN(query,limit)
+        console.log(result)
+        if(result){
+            return res.send(result)
+        }else{
+            return createResponse(res,"products not found",undefined,404)
+        }
+    }else{
+        return createResponse(res,"Parameter not found",undefined,500)
+    }
+}
--- src/Routes/product.routes.ts
+++ src/Routes/product.routes.ts
@@ -6,3 +6,18 @@ export let Product_Router = express.Router()
 Product_Router.all("/info",(req:Request,res:Response)=>{
     return createResponse(res,"CRUD API For handling Farmer Product operations",{},200)
 })
+
+//post host/product
+Product_Router.post("",productcontroller.createProduct)
+// get host/product
+Product_Router.get("",productcontroller.getAllProducts)
+//get host/product/find
+Product_Router.get("/find",productcontroller.getProductbyId)
+//post host/product/find
+Product_Router.post("/find",productcontroller.getProducts)
+//post host/product/find/:limit
+Product_Router.post("/find/:limit",productcontroller.getNProducts)
+//post host/product/update [not all fields TODO breakdown]
+Product_Router.post("/update",productcontroller.updateProduct)
+//delete host/product/:id
+Product_Router.delete("/:id",productcontroller.deleteProduct)
