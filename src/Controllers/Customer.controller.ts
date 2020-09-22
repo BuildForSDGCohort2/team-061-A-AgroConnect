9e63075596d211d3edfcd8920d70f92b965a1c03
Chibueze-20 <analogbeichibueze@gmail.com>
2020-09-22 11:07:34 +0100

    format correction

--- src/Controllers/Customer.controller.ts
+++ src/Controllers/Customer.controller.ts
@@ -1,6 +1,6 @@
-import {CustomerRepository} from "../repositories/Customer.repository"
-import {Request, Response} from "express"
-import { Customer } from "../entities/Customer";    
+import { CustomerRepository } from "../repositories/Customer.repository"
+import { Request, Response } from "express"
+import { Customer } from "../entities/Customer";
 import { getModelForClass } from "@typegoose/typegoose";
 
 const repository = new CustomerRepository(getModelForClass(Customer))
@@ -13,7 +13,7 @@ export let deleteCustomer = async (req: Request, res: Response) => {
     return res.send(`Deleted ${id} Successfully`)
 }
 
-export let updateCustomer = async (req:Request, res: Response) => {
+export let updateCustomer = async (req: Request, res: Response) => {
     const id = String(req.params.id)
     const update = req.body
     const result = await repository.update(id, update)
@@ -24,16 +24,16 @@ export let updateCustomer = async (req:Request, res: Response) => {
     return res.send(result)
 }
 
-export let getAllCustomers = async (req:Request, res:Response) => {
+export let getAllCustomers = async (req: Request, res: Response) => {
     const query = {}
     const result = await repository.find(query)
     if (!result) return res.status(400).send("No customer Available")
     return res.send(result)
 }
 
-export let getCustomerById = async (req:Request, res:Response) => {
+export let getCustomerById = async (req: Request, res: Response) => {
     const id = req.query.id
-    const result = await repository.findOne({_id: id})
+    const result = await repository.findOne({ _id: id })
     if (!result) return res.status(400).send("Invalid Customer")
     return res.send(result)
 }
@@ -47,24 +47,24 @@ export let getCustomers = async (req: Request, res: Response) => {
         console.log("done")
         return res.send(result)
     } else {
-        return res.status(404).send({message: "User not found"})
+        return res.status(404).send({ message: "User not found" })
     }
 }
 
-export let getNCustomers = async (req:Request,res:Response)=>{
+export let getNCustomers = async (req: Request, res: Response) => {
     const query = req.body
     // console.log(query)
-    if (req.params.limit !="") {
+    if (req.params.limit != "") {
         const limit = Number(req.params.limit)
-        const result = await repository.findN(query,limit)
+        const result = await repository.findN(query, limit)
         console.log(result)
-        if(result){
+        if (result) {
             return res.send(result)
         } else {
-            return res.status(404).send({message: "User not found"})
+            return res.status(404).send({ message: "User not found" })
         }
-} else {
-    return res.status(500).send({message: "Parameter not found"})
-}
+    } else {
+        return res.status(500).send({ message: "Parameter not found" })
+    }
 
 }
--- src/Controllers/Tag.controller.ts
+++ src/Controllers/Tag.controller.ts
@@ -1,32 +1,32 @@
-import {TagRepository} from "../repositories/Tag.repository"
-import {Request, Response} from "express"
+import { TagRepository } from "../repositories/Tag.repository"
+import { Request, Response } from "express"
 import { Tag } from "../entities/Tag";
-import {getModelForClass} from '@typegoose/typegoose'
-import {createResponse} from "../Utils/Response.custom"
+import { getModelForClass } from '@typegoose/typegoose'
+import { createResponse } from "../Utils/Response.custom"
 
 
 
 const repository = new TagRepository(getModelForClass(Tag))
 
-export let createTag = async (req: Request, res: Response) =>{
+export let createTag = async (req: Request, res: Response) => {
     const Tag = req.body
     const result = await repository.create(Tag)
     if (result) {
-        const newTag = await repository.findOne({_id:result})
-        return createResponse(res,"Tag created",newTag,200)
-    }else{
-        return createResponse(res,"Error creating Tag in db",undefined,500)
+        const newTag = await repository.findOne({ _id: result })
+        return createResponse(res, "Tag created", newTag, 200)
+    } else {
+        return createResponse(res, "Error creating Tag in db", undefined, 500)
     }
 }
 
-export let deleteTag = async (req:Request, res:Response) => {
+export let deleteTag = async (req: Request, res: Response) => {
     const id = req.params.id
     const result = await repository.delete(id)
     if (!result) return res.status(400).send('Bad request. Operation failed')
     return res.send(`Delete ${id} Successfully`)
 }
 
-export let updateTag = async (req:Request, res:Response) => {
+export let updateTag = async (req: Request, res: Response) => {
     const id = String(req.params.id)
     const updateTag = req.body
     const result = await repository.update(id, updateTag)
@@ -36,7 +36,7 @@ export let updateTag = async (req:Request, res:Response) => {
     return res.send(`Update ${id} Successful`)
 }
 
-export let getAllTag = async (req:Request, res:Response) => {
+export let getAllTag = async (req: Request, res: Response) => {
     const query = {}
     const result = repository.find(query)
     if (!result) return res.status(400).send('No Tags Available')
@@ -44,9 +44,9 @@ export let getAllTag = async (req:Request, res:Response) => {
 
 }
 
-export let getTagById = async (req:Request, res:Response) => {
+export let getTagById = async (req: Request, res: Response) => {
     const id = req.query.id
-    const result = await repository.findOne({_id: id})
+    const result = await repository.findOne({ _id: id })
     if (!result) return res.status(400).send("Invalid Tag")
     return res.send(result)
 }
@@ -60,24 +60,24 @@ export let getTags = async (req: Request, res: Response) => {
         console.log("done")
         return res.send(result)
     } else {
-        return res.status(404).send({message: "Tag not found"})
+        return res.status(404).send({ message: "Tag not found" })
     }
 }
 
-export let getNTags = async (req:Request,res:Response)=>{
+export let getNTags = async (req: Request, res: Response) => {
     const query = req.body
     // console.log(query)
-    if (req.params.limit !="") {
+    if (req.params.limit != "") {
         const limit = Number(req.params.limit)
-        const result = await repository.findN(query,limit)
+        const result = await repository.findN(query, limit)
         console.log(result)
-        if(result){
+        if (result) {
             return res.send(result)
         } else {
-            return res.status(404).send({message: "Tag not found"})
+            return res.status(404).send({ message: "Tag not found" })
         }
-} else {
-    return res.status(500).send({message: "Parameter not found"})
-}
+    } else {
+        return res.status(500).send({ message: "Parameter not found" })
+    }
 
 }
