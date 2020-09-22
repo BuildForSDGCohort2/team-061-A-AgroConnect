12dc42e84f87ea21adae473c1057ac63f207e6ca
Chibueze-20 <analogbeichibueze@gmail.com>
2020-09-22 09:27:38 +0100

    update to farmer contorller and route

--- src/Controllers/Farmer.controller.ts
+++ src/Controllers/Farmer.controller.ts
@@ -44,25 +44,53 @@ export let getFarmerbyId = async (req:Request,res:Response) =>{
         return res.status(404).send("User not found")
     }
 }
-export let getFarmers = async (req:Request,res:Response)=>{
+export let getFarmers = async (req: Request, res: Response) => {
     const query = req.body
     // console.log(query)
+    const result = await repository.find(query)
+    console.log(Boolean(result))
+    if (result) {
+        console.log("done")
+        return res.send(result)
+    } else {
+        return res.status(404).send({message:"User not found"})
+    }
+}
+export let getNFarmers = async (req: Request, res:Response)=>{
+    const query = req.body
     if (req.params.limit !="") {
         const limit = Number(req.params.limit)
         const result = await repository.findN(query,limit)
         console.log(result)
         if(result){
             return res.send(result)
-        }
-    } else {
-        const result = await repository.find(query)
-        console.log(Boolean(result))
-        
-        if(result){
-            console.log("done")
-            return res.send(result)
         }else{
             return res.status(404).send({message:"User not found"})
         }
+    }else{
+        return res.status(500).send({message:"Parameter not found"})
     }
 }
+// ! OLD version, used the 2 function version, update all that have the same pattern
+// export let getFarmers = async (req:Request,res:Response)=>{
+//     const query = req.body
+//     // console.log(query)
+//     if (req.params.limit !="") {
+//         const limit = Number(req.params.limit)
+//         const result = await repository.findN(query,limit)
+//         console.log(result)
+//         if(result){
+//             return res.send(result)
+//         }
+//     } else {
+//         const result = await repository.find(query)
+//         console.log(Boolean(result))
+        
+//         if(result){
+//             console.log("done")
+//             return res.send(result)
+//         }else{
+//             return res.status(404).send({message:"User not found"})
+//         }
+//     }
+// }
