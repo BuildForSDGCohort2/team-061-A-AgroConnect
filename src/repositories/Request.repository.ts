bf32acc3ebffe8036565bf1cdfd98d013f693e92
mjaynixxz <mmuhammadjamiu@gmail.com>
2020-09-30 19:15:09 +0100

    request repository

--- src/repositories/Request.repository.ts
+++ src/repositories/Request.repository.ts
@@ -1,6 +1,20 @@
 import {BaseRepository} from "./base/Base.repository"
 import { Request } from "../entities/Request";
+import {getModelForClass} from '@typegoose/typegoose';
 
 export class RequestRepository extends BaseRepository<Request>{
+   
+        // model = getModelForClass(Bid);
+        
+        
+        async getRequestByProduct(Product:any):Promise<Bid[]> {
+            const result = await this.model.find({products: Product}).populate("products").sort({products: 1})
+            return result
+        }
+    
+        async getRequestByCustomer(Customer:any):Promise<Bid[]>{
+            const result = await this.model.find({customer: Customer}).populate("customer").sort({customer: 1})
+            return result
+        } 
     
 }
