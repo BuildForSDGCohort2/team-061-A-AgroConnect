29e5cd6ed429a6e106acf96cb3fe03153e76652d
Chibueze-20 <analogbeichibueze@gmail.com>
2020-09-30 23:26:06 +0100

    merge commit

--- Backend/build/repositories/FarmerRepository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FarmerRepository = void 0;
-const BaseRepository_1 = require("./base/BaseRepository");
-class FarmerRepository extends BaseRepository_1.BaseRepository {
-}
-exports.FarmerRepository = FarmerRepository;
