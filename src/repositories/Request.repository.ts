b459ad43fee8990254a70c90267fbb8edcf71682
mjaynixxz <mmuhammadjamiu@gmail.com>
2020-09-30 19:19:31 +0100

    update request repository

--- src/repositories/Request.repository.ts
+++ src/repositories/Request.repository.ts
@@ -4,15 +4,15 @@ import {getModelForClass} from '@typegoose/typegoose';
 
 export class RequestRepository extends BaseRepository<Request>{
    
-        // model = getModelForClass(Bid);
+        // model = getModelForClass(Request);
         
         
-        async getRequestByProduct(Product:any):Promise<Bid[]> {
+        async getRequestByProduct(Product:any):Promise<Request[]> {
             const result = await this.model.find({products: Product}).populate("products").sort({products: 1})
             return result
         }
     
-        async getRequestByCustomer(Customer:any):Promise<Bid[]>{
+        async getRequestByCustomer(Customer:any):Promise<Request[]>{
             const result = await this.model.find({customer: Customer}).populate("customer").sort({customer: 1})
             return result
         } 
